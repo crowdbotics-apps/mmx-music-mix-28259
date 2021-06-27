2b72ed174f82c988434f11333646a66ecc7392a2
Crowdbotics <team@crowdbotics.com>
2021-06-27 22:13:47 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mmx_music_mix_28259/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mmx_music_mix_28259;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "mmx_music_mix_28259";
+  }
+}
